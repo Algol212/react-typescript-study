babdf43d7a20975588976d49bb15d57c514cf30d
Algol212 <99543986+Algol212@users.noreply.github.com>
2022-06-23 22:16:09 +0900

    Initial commit

---
+++ src/Todo.tsx
@@ -0,0 +1,5 @@
+export const Todo = (props) => {
+  const { title, userid, completed } = props;
+  const completedMark = completed ? "[完]" : "[未]";
+  return <p>{`${completedMark} ${title}(ユーザー:${userid})`}</p>;
+};
