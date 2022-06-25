00a8c3896d68c302e327145f268c7e98b957259f
Algol212 <99543986+Algol212@users.noreply.github.com>
2022-06-26 00:56:54 +0900

    props、コンポーネントの型定義、オプショナルチェイニング

--- src/Todo.tsx
+++ src/Todo.tsx
@@ -1,5 +1,8 @@
-export const Todo = (props) => {
-  const { title, userid, completed } = props;
+import { VFC } from "react";
+import { TodoType } from "./types/todo";
+
+export const Todo: VFC<Omit<TodoType, "id">> = (props) => {
+  const { title, userId, completed = false } = props;
   const completedMark = completed ? "[完]" : "[未]";
-  return <p>{`${completedMark} ${title}(ユーザー:${userid})`}</p>;
+  return <p>{`${completedMark} ${title}(ユーザー:${userId})`}</p>;
 };
